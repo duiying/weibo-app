ce16178234d1c4da5165ede46e7f9028a8888315
wangyaxian <wangyaxian@higohappy.com>
2019-09-27 22:16:23 +0800

    MOD:保存用户提交的表单数据

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -36,6 +36,13 @@ class UsersController extends Controller
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return redirect()->route('users.show', [$user]);
     }
 }
