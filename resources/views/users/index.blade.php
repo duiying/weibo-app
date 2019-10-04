c7c36f02902f91d22e7b3aaa89a29866df258135
wangyaxian <wangyaxian@higohappy.com>
2019-10-04 19:41:49 +0800

    用户列表

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.default')
+@section('title', '所有用户')
+@section('content')
+    <div class="offset-md-2 col-md-8">
+        <h2 class="mb-4 text-center">所有用户</h2>
+        <div class="list-group list-group-flush">
+            @foreach ($users as $user)
+                <div class="list-group-item">
+                    <img class="mr-3" src="{{ $user->gravatar() }}" alt="{{ $user->name }}">
+                    <a href="{{ route('users.show', $user) }}">
+                        {{ $user->name }}
+                    </a>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@stop
