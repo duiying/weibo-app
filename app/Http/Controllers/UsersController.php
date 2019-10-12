8ae5087dcc2f5d1cedbf981842e3a38b87c16482
wangyaxian <wangyaxian@higohappy.com>
2019-10-12 23:18:26 +0800

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -61,7 +61,8 @@ class UsersController extends Controller
      */
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()->orderBy('created_at', 'desc')->paginate(10);
+        return view('users.show', compact('user', 'statuses'));
     }
 
     /**
